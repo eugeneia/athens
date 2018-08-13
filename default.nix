f121f174bc57a2cd66ede0fc2d79a9007ca3afcc
Max Rottenkolber <max@mr.gy>
2018-08-13 16:00:06 +0200

    default.nix: add Nix expression for Athens.

---
+++ default.nix
@@ -0,0 +1,30 @@
+# Run like this:
+#   nix-build /path/to/this/directory
+# ... build products will be in ./result
+
+{ pkgs ? (import <nixpkgs> {}), source ? ./., version ? "dev" }:
+
+with pkgs;
+
+stdenv.mkDerivation rec {
+  name = "athens-${version}";
+  src = lib.cleanSource source;
+
+  buildInputs = [ makeWrapper ccl openssl libxml2 ];
+  inherit version openssl libxml2;
+
+  preBuild = ''
+    make clean
+    export LD_LIBRARY_PATH=${lib.makeLibraryPath [ openssl libxml2 ]}
+    export XDG_CACHE_HOME="$TMP/.cache"
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp bin/athens $out/bin/athens.orig
+    makeWrapper $out/bin/athens.orig $out/bin/athens \
+        --suffix LD_LIBRARY_PATH : $LD_LIBRARY_PATH
+  '';
+
+  dontStrip = true;
+}
